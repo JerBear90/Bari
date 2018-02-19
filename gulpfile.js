337190adda086c11bfbe3f41858cf5a7ddfdd320
Jeramee <jerameeflemming@gmail.com>
2018-02-19 13:55:25 -0500

    Reworking gulp to watch set files

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,22 @@ var gulp = require('gulp'),
   pug = require('gulp-pug'),
   sass = require('gulp-sass'),
   watch = require('gulp-watch'),
-  gulp_watch_pug = require('gulp-watch-pug');
+  gulp_watch_pug = require('gulp-watch-pug'),
+  gulpPugBeautify = require('gulp-pug-beautify');
+
+// gulp.task('default', function () {
+//   return gulp.src('build/*.html')
+//
+//     .pipe(gulp.dest('build/'));
+// });
 
 // run this task by typing in gulp pug in CLI
-gulp.task('pug', function() {
+gulp.task('pug', function buildHTML(){
   return gulp.src('templates/*.pug')
       .pipe(pug()) // pipe to pug plugin
+      .pipe(gulpPugBeautify({ omit_empty: true }))
       .pipe(gulp.dest('build')); // tell gulp our output folder
 });
-
 
 // Gulp Sass
 gulp.task('styles', function() {
@@ -29,6 +36,7 @@ gulp.src('templates/**/*.pug')
     .pipe(watch('templates/**/*.pug'))
     .pipe(gulp_watch_pug('templates/**/*.pug', { delay: 100 }))
     .pipe(pug())
+    .pipe(gulpPugBeautify({ omit_empty: true }))
     .pipe(gulp.dest('build/'));
 
 
